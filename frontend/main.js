33c279a7dc09ea4b42e60f64e68830ff7c127307
krishpranav <krisna.pranav@gmail.com>
2022-06-02 17:31:27 +0530

    frontend + chat server update

---
+++ frontend/main.js
@@ -0,0 +1,34 @@
+// ui functionality
+const input = document.querySelector('#textarea')
+const messages = document.querySelector('#messages')
+const username = document.querySelector('#username')
+const send = document.querySelector('#send')
+
+const url = "ws://" + window.location.host + "/chat";
+const ws = new WebSocket(url);
+
+ws.onmessage = function(msg) {
+    insertMessage(JSON.parse(msg.data));
+};
+
+send.onclick = () => {
+    const message = {
+        username: username.value,
+        content: input.value,
+    }
+
+    ws.send(JSON.stringify(message));
+    input.value = "";
+};
+
+function insertMessage(messageObj) {
+    const message = document.createElement('div')
+
+    message.setAttribute('class', 'chat-message')
+    console.log("name: "+messageObj.username + " content: " + messageObj.content)
+    message.textContent = `${messageObj.username}: ${messageObj.content}`
+
+    messages.appendChild(message)
+
+    messages.insertBefore(message, messages.firstChild)
+}
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+	"gopkg.in/olahol/melody.v1"
+)
+
+func main() {
+	r := gin.Default()
+	m := melody.New()
+
+	r.Use(static.Serve("/", static.LocalFile("./frontend", true)))
+
+	r.GET("/chat", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		m.Broadcast(msg)
+	})
+
+	r.Run(":8080")
+}
